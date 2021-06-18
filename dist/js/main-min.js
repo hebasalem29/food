547039733550370db36bee5b2ace6257403cac48
hebasalem29 <hebanassr58@gmail.com>
2021-06-18 19:11:32 +0200

    caro done elhamdullah

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){$(window).on("scroll",function(){0==$(window).scrollTop()?($(".navbar").addClass("co"),console.log("hi")):$(".navbar").removeClass("co")})});
+$(function(){$(window).on("scroll",function(){0==$(window).scrollTop()?($(".navbar").addClass("co"),console.log("hi")):$(".navbar").removeClass("co")}),$(".owl-carousel").owlCarousel({loop:!0,margin:10,nav:!0,autoplay:!0,autoplayTimeout:3e3,autoplayHoverPause:!0,responsive:{0:{items:1},600:{items:2},1000:{items:3}}})});
